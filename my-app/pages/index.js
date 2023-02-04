984a4f1c3c02f6c9d9d5a4bbae1b09c44f6261b3
Hector-V12 <h.paco@orange.fr>
2023-02-04 20:40:15 +0100

    Ethers

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -4,6 +4,7 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Web3Modal from 'web3modal';
+const { ethers } = require('ethers');
 import { providers, Contract } from 'ethers';
 import { useEffect, useRef, useState } from 'react';
 import { WHITELIST_CONTRACT_ADDRESS, abi } from '../constants';
